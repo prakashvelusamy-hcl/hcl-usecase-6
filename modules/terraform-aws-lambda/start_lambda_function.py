7aa69808c26b6bd823c5fba56fcf84d6ce4b1b74
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-06 14:28:30 +0530

    terraform apply

--- modules/terraform-aws-lambda/start_lambda_function.py
+++ modules/terraform-aws-lambda/start_lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import os
 region = 'ap-south-1'
 #instances=['i-0f834339c52a6e0a9']
-instances = os.environ('INSTANCE_ID')
+instances = os.enviro.get('INSTANCE_ID')
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):

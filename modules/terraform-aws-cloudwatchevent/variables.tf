650069cbfab5af71381e2dabfac0b0fc3094c4dd
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-06 14:55:11 +0530

    terraform destroy

--- modules/terraform-aws-cloudwatchevent/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "instance_stop_time"{
-description = " The time instance need to be stopped"
-type = string
-}
-variable "instance_start_time" {
-description = " The time instance need to be start"
-type = string
-}
